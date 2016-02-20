dbb419139d91702798d7acd46871c17dfec480f6
FiveClicks <lovely2days@gmail.com>
2016-02-20 13:10:41 -0700

    CSE360assign3 question 1

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,49 @@
+/*
+ * @author: Kartik Gupta
+ * @version 2/20/2016
+ * 
+ * Project description: Skeleton of the calculator class. This class will be implemented
+ * further in other parts.
+ * 
+ */
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	// Constructor for the calculator class.
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	// returns the total of the calculator
+	public int getTotal () {
+		return 0;
+	}
+	
+	//Performs the add function of a calculator
+	public void add (int value) {
+		
+	}
+	
+	//performs the subtract function of a calculator
+	public void subtract (int value) {
+		
+	}
+	
+	//performs the multiply function of a calculator
+	public void multiply (int value) {
+		
+	}
+	
+	//performs the divide function of a calculator
+	public void divide (int value) {
+		
+	}
+	
+	//Returns the history of the calculator
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,39 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testCalculator() {
+		Calculator calc = new Calculator();
+		assertNotNull(calc);
+	}
+
+	public void testgetTotal() {
+		fail("Not yet implemented");
+	}
+	
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+	
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+	
+	public void testMultiply() {
+		fail("Not yet implemented");
+	}
+	
+	public void testDivide() {
+		fail("Not yet implemented");
+	}
+	
+	public void testGetHistory() {
+		fail("Not yet implemented");
+	}
+	
+}

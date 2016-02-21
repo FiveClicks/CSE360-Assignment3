5bcd04677af90b6728f67fd7cc74cf7a1d1502f4
FiveClicks <lovely2days@gmail.com>
2016-02-21 12:11:23 -0700

    Third Version part 2. Added comments to J Unit test file.

--- cse360assign3/src/cse360assign3/CalculatorTest.java
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -1,3 +1,8 @@
+/*
+ * @Author: Kartik Gupta
+ * @Version 3
+ * 
+ */
 package cse360assign3;
 
 import static org.junit.Assert.*;
@@ -6,32 +11,37 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
+	// Tests the calculator constructor
 	@Test
 	public void testCalculator() {
 		Calculator calc = new Calculator();
 		assertNotNull(calc);
 	}
 	
+	//Tests the getTotal function
 	@Test
 	public void testgetTotal() {
 		Calculator calc = new Calculator();
 		assertEquals(0, calc.getTotal());
 	}
 	
+	// Tests the add function
 	@Test
 	public void testAdd() {
 		Calculator calc = new Calculator();
 		calc.add(3);
 		assertEquals(3, calc.getTotal());
 	}
-	
+
+	// Tests the subtract function
 	@Test
 	public void testSubtract() {
 		Calculator calc = new Calculator();
 		calc.subtract(5);
 		assertEquals(-5, calc.getTotal());
 	}
-	
+
+	// Tests the multiply function
 	@Test
 	public void testMultiply() {
 		Calculator calc = new Calculator();
@@ -39,7 +49,8 @@ public class CalculatorTest {
 		calc.multiply(5);
 		assertEquals(50, calc.getTotal());
 	}
-	
+
+	// Tests the divide function
 	@Test
 	public void testDivide() {
 		Calculator calc = new Calculator();
@@ -47,14 +58,17 @@ public class CalculatorTest {
 		calc.divide(2);
 		assertEquals(4, calc.getTotal());
 	}
-	
+
+	// Tests the divide by zero function
 	@Test
 	public void testDividebyZero() {
 		Calculator calc = new Calculator();
+		calc.add(2);
 		calc.divide(0);
 		assertEquals(0, calc.getTotal());
 	}
 	
+	// Tests the getHistory function
 	@Test
 	public void testGetHistory() {
 		Calculator calc = new Calculator();
@@ -64,7 +78,8 @@ public class CalculatorTest {
 		calc.add(5);
 		assertEquals("0 + 4 - 2 * 2 + 5", calc.getHistory());
 	}
-	
+
+	// Tests the getHistory function with different values and more functions
 	@Test
 	public void testGetHistory2() {
 		Calculator calc = new Calculator();
@@ -75,7 +90,8 @@ public class CalculatorTest {
 		calc.add(5);
 		assertEquals("0 + 10 * 6 - 5 / 11 + 5", calc.getHistory());
 	}
-	
+
+	// Tests the getHistory function to see if the values are not the same
 	@Test
 	public void testGetHistory3() {
 		Calculator calc = new Calculator();
